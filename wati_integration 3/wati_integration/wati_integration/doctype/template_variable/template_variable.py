30bbeb4c6d30fc03757d0f53e540bbfcf3be08d0
skyasir <168763255+skyasir@users.noreply.github.com>
2026-06-02 20:12:20 +0530

    Delete wati_integration 3 directory

--- wati_integration 3/wati_integration/wati_integration/doctype/template_variable/template_variable.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2022, Yasir Shaikh and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-class TemplateVariable(Document):
-	pass
