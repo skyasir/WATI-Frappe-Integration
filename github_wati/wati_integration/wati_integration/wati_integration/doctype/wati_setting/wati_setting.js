971d338b81a2a97e2f2146cc8174508c7979bf86
skyasir <168763255+skyasir@users.noreply.github.com>
2026-06-02 16:51:57 +0530

    Delete github_wati/wati_integration directory

--- github_wati/wati_integration/wati_integration/wati_integration/doctype/wati_setting/wati_setting.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2022, Yasir Shaikh and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Wati Setting', {
-	// refresh: function(frm) {
-
-	// }
-});
